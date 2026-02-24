5e2ef5d5f93efb27b4baa6fb2e4f40fbb3f22699
tejaswi <tejaswi0623@gmail.com>
2026-02-24 17:38:21 -0500

    message

--- mongodb.sh
+++ mongodb.sh
@@ -23,3 +23,19 @@ systemctl restart mongod
 validate $? "restart mongodb"
 
 print_total_time
+
+
+#sudo sh mongodb.sh
+#2026-02-24 22:35:25 | Script started executing at: Tue Feb 24 22:35:25 UTC 2026
+#2026-02-24 22:35:25| copying mongo repo is...... success
+#MongoDB Repository                                                                                                             17 kB/s | 1.3 kB     00:00
+#Package mongodb-org-7.0.30-1.el9.x86_64 is already installed.
+#Dependencies resolved.
+#Nothing to do.
+#Complete!
+#2026-02-24 22:35:32| installing mongodb is...... success
+#2026-02-24 22:35:32| enable and start mongodb is...... success
+#2026-02-24 22:35:32| allow remote connections is...... success
+#2026-02-24 22:35:33| restart mongodb is...... success
+#2026-02-24 22:35:33 | Script execute in:  8 seconds
+#above is the output we got with exact time and date stamp and also we got total time taken to execute the script
