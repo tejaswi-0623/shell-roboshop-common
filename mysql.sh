259e186f1e29616dfe58acb32cde14d67accf9fa
tejaswi <tejaswi0623@gmail.com>
2026-02-25 13:19:33 -0500

    message

--- mysql.sh
+++ mysql.sh
@@ -3,12 +3,12 @@
 source ./common.sh #calling common script
 check_rootuser
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$logs_file
 validate $? "installing mysql"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$logs_file
 systemctl start mysqld
 validate $? "enable and start mysql"
 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$logs_file
 validate $? "setting root user password"
