256b010b99370c49577364809a8942e47b3c4578
tejaswi <tejaswi0623@gmail.com>
2026-02-25 14:49:04 -0500

    message

--- payment.sh
+++ payment.sh
@@ -12,5 +12,9 @@ systemctl_services
 cp $script_dir/payment.service /etc/systemd/system/$appname.service
 validate $? "copyting payment service"
 
+
+pip3 install -r requirements.txt &>>$logs_file
+validate $? "intsalling dependencies"
+
 systemctl restart $appname &>>$logs_file
 validate $? "restarting $appname"
