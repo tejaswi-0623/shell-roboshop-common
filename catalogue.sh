af0eb4cdba3c29a64eea8ca775854f0f520358de
tejaswi <tejaswi0623@gmail.com>
2026-02-25 12:54:57 -0500

    message

--- catalogue.sh
+++ catalogue.sh
@@ -13,11 +13,9 @@ print_total_time
 
 
 
-#npm install &>>$logs_file
-#validate $? "installing dependencies"
+npm install &>>$logs_file
+validate $? "installing dependencies"
 
-systemctl daemon-reload
-validate $? "reload service"
 
 cp $script_dir/mongodb.repo /etc/yum.repos.d/mongo.repo
 validate $? "copying the mongo repo"
--- common.sh
+++ common.sh
@@ -43,7 +43,6 @@ nodejs_setup(){ #writting function for coomon commands for nodejs in backend scr
 
     dnf install nodejs -y &>>$logs_file
     validate $? "intsalling nodejs"
-
     
 }
 
